97d392d99892361458fd853da5ef97c6c923ae0f
KDragon <elkarouani@gmail.com>
2019-03-24 22:01:42 +0100

    reading and converting image to grayscale

--- simple.py
+++ simple.py
@@ -7,4 +7,7 @@ cascade_path = "haarcascade_frontalface_default.xml"
 # Initialise the Haar CascadeClassifier with the XML file
 haar_face_cascade = cv2.CascadeClassifier(cascade_path)
 
+# Read the photo and convert to grayscale
+photo = cv2.imread(photo_path)
+grayscale = cv2.cvtColor(photo, cv2.COLOR_BGR2grayscale)
 
