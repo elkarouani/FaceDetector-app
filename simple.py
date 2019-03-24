91ad97fc9ee8458c9e896ed2468499c704a49cf4
KDragon <elkarouani@gmail.com>
2019-03-24 22:00:27 +0100

    Initializing haar cascade xml file

--- simple.py
+++ simple.py
@@ -1,1 +1,10 @@
 import cv2
+
+# Specify the image path for face detection and XML file for the cascade
+photo_path = "family.jpg"
+cascade_path = "haarcascade_frontalface_default.xml"
+
+# Initialise the Haar CascadeClassifier with the XML file
+haar_face_cascade = cv2.CascadeClassifier(cascade_path)
+
+
