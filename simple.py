63cc83ae6d6f94041bd20c5b2478f1d563f28212
KDragon <elkarouani@gmail.com>
2019-03-25 23:51:11 +0100

    Fix some code

--- simple.py
+++ simple.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 # Specify the image path for face detection and XML file for the cascade
 photo_path = "family.jpg"
@@ -9,14 +10,20 @@ haar_face_cascade = cv2.CascadeClassifier(cascade_path)
 
 # Read the photo and convert to grayscale
 photo = cv2.imread(photo_path)
-grayscale = cv2.cvtColor(photo, cv2.COLOR_BGR2grayscale)
-
+grayscale = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the photo using OpenCV library
 faces = haar_face_cascade.detectMultiScale(
-        grayscale,
-        scaleFactor = 1.1,
-        minNeighbors = 5,
-        minSize = (30, 30)
-        )
+    grayscale,
+    scaleFactor = 1.1,
+    minNeighbors = 5,
+    minSize = (40, 40)
+    )
 
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces :
+    cv2.rectangle(photo, (x,y), (x+w, y+h), color = (0, 255, 0), thickness = 2)
+
+cv2.imshow("Facses found", photo)
+cv2.waitKey(0);
+                         
